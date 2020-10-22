2a20d9e4055bc26a0999756b5c56a3ebc33469ff
seunghee-ryu <shee.ryu88@gmail.com>
2020-10-22 13:57:48 +0900

    entry, start, player.json

--- mini-project-server/src/main/java/com/eomcs/pms/handler/StartCommand.java
+++ mini-project-server/src/main/java/com/eomcs/pms/handler/StartCommand.java
@@ -9,16 +9,28 @@ public class StartCommand implements Command {
   @Override
   public void execute(PrintWriter out, BufferedReader in) {
     out.println("-------------------------------------------------------------------------------------------------");
-    out.println("게임을 시작하기에 앞서 플레이어 등록을 하겠습니다.");
-    out.println("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -");
-    out.println("새로 시작하는 플레이어라면");
-    out.println("'/player/add' 명령어를 입력하세요.");
-    out.println("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -");
-    out.println("기존에 플레이 기록을 가지고 있는 플레이어라면");
-    out.println("'/player/list' 명령어를 입력해서 플레이 기록을 확인하세요.");
-    out.println("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -");
-    out.println("플레이 기록의 숫자가 당신이 있던 스테이지입니다.");
-    out.println("예) 플레이 기록이 3이라면 '/three' 명령어를 입력하세요.");
+    out.println("연쇄살인마가 살고 있다는 소문의 버려진 건물.");
+    out.println("모든 벽과 방이 붉은 색으로 칠해져 있어 사람들은 모두 이 건물을 ‘붉은 집’이라 부른다.");
+    out.println("늘 난 이 소문의 정체를 파헤치겠다는 친구 진영이에게 못 이겨 함께 이 붉은 집에 들어가기로 하였다.");
+    out.println("지금은 11시이고 만나기로 한 시간은 새벽 3시...");
+    out.println("진영이가 전화번호를 알고 있어 약속 시각에 맞춰 깨워줄 거라는 생각에 그만 잠들어버렸다.");
+    out.println(".");
+    out.println(".");
+    out.println(".");
+    out.println(".");
+    out.println(".");
+    out.println("나를 깨운 괘종시계와 침대만 덩그러니 놓여 있는 이 방은, 벽과 천장이 모두 피처럼 붉은색이다.");
+    out.println("방 뒤쪽에는 작은 문이 하나 보인다. 옷장으로 통하는 문일까?");
+    out.println("붉은색으로 칠해져 있는 벽, 음산한 소리의 괘종시계.");
+    out.println("모두 소문의 집과 똑같다.");
+    out.println(".");
+    out.println(".");
+    out.println(".");
+    out.println(".");
+    out.println(".");
+    out.println("진영이는 어디 있지?");
+    out.println("그 전에, 나는 이 집에 혼자 있을까?");
+    out.println("'/entry' 명령어를 입력하세요.");
     out.println("-------------------------------------------------------------------------------------------------");
 
   }
